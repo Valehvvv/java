88b548cc16e89c367d1b397a1f863752f172d95d
Valentina <valentina.hvvv@gmail.com>
2023-09-06 10:38:01 -0300

    jaava

--- src/Main.java
+++ src/Main.java
@@ -7,23 +7,6 @@ public class Main{
            m
          */
         System.out.println("hola mundo!!");
-        String myString = "esto es una cadena de texto";
-        myString= "cambio de texto";
-        System.out.println(myString);
-
-        Integer myInt= 7 ;
-        myInt = myInt + 4;
-        System.out.println(myInt);
-        System.out.println(myInt -1);
-
-        Double myDouble = 6.5;
-        System.out.println(myDouble);
-
-        Float myFloat = 6.5f;
-        System.out.println(myFloat);
-
-        System.out.println(myDouble+myFloat);
-
 
     }
 }
